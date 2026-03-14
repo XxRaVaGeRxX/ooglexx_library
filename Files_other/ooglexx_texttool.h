13b61f865154899309cada9a2960768c6f356958
XxRaVaGeRxX <RSOfficial4888@outlook.com>
2026-03-14 01:30:33 -0400

    changed file structure for usability

---
+++ Files_other/ooglexx_texttool.h
@@ -0,0 +1,16 @@
+#ifndef OOGLEXXTEXTTOOL_H
+#define OOGLEXXTEXTTOOL_H
+
+void b_type(const char* text);
+
+void no_newl(const char* text);
+
+void e_type(const char* text);
+
+void q_type(const char* text);
+
+void falseloadf();
+
+void falseload();
+
+#endif // OOGLEXXTEXTTOOL_H
